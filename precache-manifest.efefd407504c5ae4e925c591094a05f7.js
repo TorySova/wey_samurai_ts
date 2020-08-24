84a779e79c0dd7d9469b986ced0d3a5e236f430d
TorySova <toryssova@gmail.com>
2020-08-25 00:30:32 +0300

    Updates

---
+++ precache-manifest.efefd407504c5ae4e925c591094a05f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d083d5aa1de223db9d64885fc3a409",
+    "url": "/wey_samurai_ts/index.html"
+  },
+  {
+    "revision": "defbce535b3b2e850f1f",
+    "url": "/wey_samurai_ts/static/css/main.e0efe4ae.chunk.css"
+  },
+  {
+    "revision": "9eefa1b24243bbc37b5c",
+    "url": "/wey_samurai_ts/static/js/2.e7724d1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wey_samurai_ts/static/js/2.e7724d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "defbce535b3b2e850f1f",
+    "url": "/wey_samurai_ts/static/js/main.83961b6e.chunk.js"
+  },
+  {
+    "revision": "40fcad7b479674c995f7",
+    "url": "/wey_samurai_ts/static/js/runtime-main.59bf36b3.js"
+  }
+]);
