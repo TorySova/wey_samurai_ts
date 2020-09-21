97d166c5f6e0795515a59f9d2f4e05561f06532b
TorySova <toryssova@gmail.com>
2020-09-21 15:41:08 +0300

    Updates

---
+++ precache-manifest.b9e0328faf7b923ad9a07bceb321992e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc85d4ab160d8762c40ac0f019fbcdb9",
+    "url": "/wey_samurai_ts/index.html"
+  },
+  {
+    "revision": "6e03e59a9876c5a5c30f",
+    "url": "/wey_samurai_ts/static/css/main.33d56315.chunk.css"
+  },
+  {
+    "revision": "aaf0cc12c5cc6734940a",
+    "url": "/wey_samurai_ts/static/js/2.6b99c879.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wey_samurai_ts/static/js/2.6b99c879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e03e59a9876c5a5c30f",
+    "url": "/wey_samurai_ts/static/js/main.b3bc7c5d.chunk.js"
+  },
+  {
+    "revision": "40fcad7b479674c995f7",
+    "url": "/wey_samurai_ts/static/js/runtime-main.59bf36b3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wey_samurai_ts/precache-manifest.d77d42a07b9cb3601c523607a568a009.js"
+  "/wey_samurai_ts/precache-manifest.b9e0328faf7b923ad9a07bceb321992e.js"
 );
 
 self.addEventListener('message', (event) => {
