434435e59861b44bd64f6c029334d68cfcdf6a3c
TorySova <toryssova@gmail.com>
2020-09-21 15:59:46 +0300

    54 componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -13,42 +13,41 @@ type PropsType = {
 
 class Users extends React.Component<PropsType> {
 
-    constructor(props:PropsType ) {
-        super(props);
-            Axios.get("https://social-network.samuraijs.com/api/1.0/users")
-                .then(response => {
-                    this.props.setUsers(response.data.items)
-                });
+    componentDidMount() {
+        Axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            });
     }
 
     render() {
         return <div>
-        {
-            this.props.usersPage.users.map(u => <div key={u.id} className={s.item}>
-                <div className={s.block1}>
-                    <div>
-                        <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={s.avatar} />
+            {
+                this.props.usersPage.users.map(u => <div key={u.id} className={s.item}>
+                    <div className={s.block1}>
+                        <div>
+                            <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={s.avatar} />
+                        </div>
+                        <div>
+                            {u.isFollow
+                                ? <button className={s.button} onClick={() => { this.props.unFollow(u.id) }}>Unfollow</button>
+                                : <button className={s.button} onClick={() => { this.props.follow(u.id) }}>Follow</button>}
+                        </div>
                     </div>
-                    <div>
-                        {u.isFollow
-                            ? <button className={s.button} onClick={() => { this.props.unFollow(u.id) }}>Unfollow</button>
-                            : <button className={s.button} onClick={() => { this.props.follow(u.id) }}>Follow</button>}
-                    </div>
-                </div>
-                <div className={s.messageBlock}>
-                    <div className={s.block2}>
-                        <div>{u.name}</div>
-                        <div>{u.status}</div>
-                    </div>
+                    <div className={s.messageBlock}>
+                        <div className={s.block2}>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </div>
 
-                    <div className={s.block3}>
-                        <div>{"u.location.country"}</div>
-                        <div>{"u.location.city"}</div>
+                        <div className={s.block3}>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
+                        </div>
                     </div>
-                </div>
-            </div>)
-        }
-    </div >
+                </div>)
+            }
+        </div >
     }
 }
 
